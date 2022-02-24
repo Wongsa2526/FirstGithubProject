976473c006b8b513aa794164c7dcbb04f1e68cf2
Wongsa2526 <tahwongsa@gmail.com>
2022-02-23 21:46:48 -0500

    add

--- src/Github_Day1/LearningGithub.java
+++ src/Github_Day1/LearningGithub.java
@@ -5,6 +5,6 @@ public class LearningGithub {
     public static void main(String[] args) {
 
         System.out.println("sumetta" +" " + "wongsa");
-        
+
     }
 }
---
+++ src/Github_Day1/Second_Java.java
@@ -0,0 +1,9 @@
+package Github_Day1;
+
+public class Second_Java {
+
+    public static void main(String[] args) {
+
+        System.out.println("test");
+    }
+}
